7147032dfa7024df90324e067618c1f3d46b240d
Yan <ianpilip@gmail.com>
2019-05-20 13:44:19 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,7 +3,7 @@ document.querySelector('button.create-new-element').addEventListener('click', ST
 document.querySelector('button.connect-elements').addEventListener('click', ST.createConnection);
 
 dragOptions = {
-	horizontal : true, // if the param will be false, nodes will not be drugged only on X coordinate
-	vertical : true // if the param will be false, nodes will not be drugged only on Y coordinate
+	horizontal : true, // if the param will be false, nodes will not be drugged only on X axis
+	vertical : true // if the param will be false, nodes will not be drugged only on Y axis
 }
 ST.drag(dragOptions);
